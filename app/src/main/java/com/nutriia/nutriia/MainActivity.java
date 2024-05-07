59c83b75944924f21983ab2d9110a527336ea8e0
lainel jim <jim.lainel@etu.univ-cotedazur.fr>
2024-05-07 09:39:05 +0200

    NavBar, Header et Drawer

--- app/src/main/java/com/nutriia/nutriia/MainActivity.java
+++ app/src/main/java/com/nutriia/nutriia/MainActivity.java
@@ -1,14 +1,25 @@
 package com.nutriia.nutriia;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ImageButton lateralOpenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,35 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        lateralOpenButton = findViewById(R.id.lateral_open);
+
+        lateralOpenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.navigation_home) {
+                    // Handle the home action
+                    return true;
+                } else if (id == R.id.navigation_dashboard) {
+                    // Handle the dashboard action
+                    return true;
+                } else if (id == R.id.navigation_notifications) {
+                    // Handle the notifications action
+                    return true;
+                }
+                return false;
+            }
         });
     }
 }
