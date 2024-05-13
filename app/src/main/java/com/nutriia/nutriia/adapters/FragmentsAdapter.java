ec81d1b48fc33c97e032978781d305e80f4896fa
louis rainero <louis.rainero@etu.unice.fr>
2024-05-13 16:19:59 +0200

    composant titre des pages

--- app/src/main/java/com/nutriia/nutriia/adapters/FragmentsAdapter.java
+++ app/src/main/java/com/nutriia/nutriia/adapters/FragmentsAdapter.java
@@ -67,6 +67,9 @@ public class FragmentsAdapter extends RecyclerView.Adapter<FragmentsAdapter.Frag
     @Override
     public void onBindViewHolder(@NonNull FragmentViewHolder holder, int position) {
         Fragment fragment = fragments.get(position);
+        if (fragment.isAdded()) {
+            return;
+        }
         fragmentManager.beginTransaction()
                 .replace(holder.getContainer().getId(), fragment)
                 .commit();
