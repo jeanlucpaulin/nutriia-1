b940a3a58343979b0b7a07b55dc98a7e67ae3313
louis rainero <louis.rainero@etu.unice.fr>
2024-05-15 16:25:04 +0200

    ChatBot sans drawer

--- app/src/main/java/com/nutriia/nutriia/activities/CoachActivity.java
+++ app/src/main/java/com/nutriia/nutriia/activities/CoachActivity.java
@@ -43,15 +43,6 @@ public class CoachActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coach);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
-        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.white));
-
         //DrawerMenu.init(this);
 
         NavBarListener.init(this, R.id.navbar_coach);
