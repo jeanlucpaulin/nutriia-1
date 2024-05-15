8bdfe9d8b068b2ef6674a33c16c0988d6d435a57
GermainDR <74007608+GermainDR@users.noreply.github.com>
2024-05-15 13:41:03 +0200

    Composant Journée Type + Composant Suggestion Plat  + corrections de probleme mineurs :construction:

--- app/src/main/java/com/nutriia/nutriia/fragments/DishSuggestions.java
+++ app/src/main/java/com/nutriia/nutriia/fragments/DishSuggestions.java
@@ -1,10 +1,14 @@
 package com.nutriia.nutriia.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +16,7 @@ import androidx.fragment.app.Fragment;
 
 import com.nutriia.nutriia.Dish;
 import com.nutriia.nutriia.R;
+import com.nutriia.nutriia.activities.DishCompositionActivity;
 import com.nutriia.nutriia.adapters.DishSuggestionAdapter;
 
 import java.util.ArrayList;
@@ -26,13 +31,24 @@ public class DishSuggestions extends Fragment {
 
         List<Dish> dishes = new ArrayList<>();
 
-        dishes.add(new Dish("Poulet grillé avec quinoa"));
+        dishes.add(new Dish("Poulet grillé avec quinoa et légumes grillés"));
+        dishes.add(new Dish("Salade de thon aux haricots et aux légumes"));
+        dishes.add(new Dish("Omelette aux légumes et fromage cottage"));
 
-        DishSuggestionAdapter adapter = new DishSuggestionAdapter(getContext(), dishes);
 
-        ListView listView = view.findViewById(R.id.dish_suggestions_list);
+        LinearLayout listView = view.findViewById(R.id.dish_suggestions_list);
 
-        listView.setAdapter(adapter);
+        for (Dish dish : dishes) {
+            View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_component_dish_suggestions, listView, false);
+
+            TextView dishName = itemView.findViewById(R.id.item_content);
+            dishName.setText(dish.getName());
+
+            ImageButton imageButton = itemView.findViewById(R.id.item_button);
+            imageButton.setOnClickListener(click -> startActivity(new Intent(getContext(), DishCompositionActivity.class)));
+
+            listView.addView(itemView);
+        }
 
         return view;
     }
--- app/src/main/java/com/nutriia/nutriia/fragments/ExampleTypicalDay.java
+++ app/src/main/java/com/nutriia/nutriia/fragments/ExampleTypicalDay.java
@@ -1,10 +1,15 @@
 package com.nutriia.nutriia.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +17,8 @@ import androidx.fragment.app.Fragment;
 
 import com.nutriia.nutriia.Meal;
 import com.nutriia.nutriia.R;
+import com.nutriia.nutriia.activities.DayAnalysisActivity;
+import com.nutriia.nutriia.activities.FoodCompositionActivity;
 import com.nutriia.nutriia.adapters.MealsAdapter;
 
 import java.util.ArrayList;
@@ -26,16 +33,37 @@ public class ExampleTypicalDay extends Fragment {
         View view = inflater.inflate(R.layout.componenent_exemple_typical_day, container, false);
 
         meals = new ArrayList<>();
-        // Exemple of a list of meals
-        //TODO : DELETE THIS EXAMPLE
-        meals.add(new Meal("Breakfast", "1 glass of orange juice 1 bowl of cereals 1 cup of coffee"));
-        meals.add(new Meal("Lunch", "1 sandwich 1 apple 1 bottle of water"));
-        meals.add(new Meal("Dinner", "1 plate of pasta 1 glass of milk 1 piece of cake"));
-        meals.add(new Meal("Snack", "1 yogurt 1 banana 1 cup of tea"));
 
-        GridView mealsListView = view.findViewById(R.id.gridView);
-        MealsAdapter mealsAdapter = new MealsAdapter(getContext(), meals);
-        mealsListView.setAdapter(mealsAdapter);
+        //TODO : DELETE THIS EXAMPLE
+        meals.add(new Meal("Petit déjeuner", "1 glass of orange juice 1 bowl of cereals 1 cup of coffee"));
+        meals.add(new Meal("Déjeuner", "1 sandwich 1 apple 1 bottle of water"));
+        meals.add(new Meal("Diner", "1 plate of pasta 1 glass of milk 1 piece of cake"));
+        meals.add(new Meal("Collations", "1 yogurt 1 banana 1 cup of tea"));
+
+        LinearLayout mealLayout = view.findViewById(R.id.mealLayout);
+        for (int i = 0; i < meals.size(); i++) {
+            LinearLayout row = (LinearLayout) mealLayout.getChildAt(i / 2);
+            View mealView = ((ViewGroup) row).getChildAt(i % 2);
+            TextView textView = mealView.findViewById(R.id.textView);
+            textView.setText(meals.get(i).getName());
+        }
+
+        Button relaunchButton = view.findViewById(R.id.relaunchButton);
+        relaunchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout mealLayout = view.findViewById(R.id.mealLayout);
+                for (int i = 0; i < meals.size(); i++) {
+                    LinearLayout row = (LinearLayout) mealLayout.getChildAt(i / 2);
+                    View mealView = ((ViewGroup) row).getChildAt(i % 2);
+                    TextView textView = mealView.findViewById(R.id.editText);
+                    textView.setText(meals.get(i).getContent());
+                }
+            }
+        });
+
+        LinearLayout layout = view.findViewById(R.id.linearLayoutMeal);
+        layout.setOnClickListener(click -> startActivity(new Intent(getContext(), DayAnalysisActivity.class)));
 
         return view;
     }
