413d177666ec6ab4d0de9937028e68bba618124b
GermainDR <germain.doglioli-ruppert@etu.unice.fr>
2024-05-17 16:42:44 +0200

    maj composition

--- app/src/main/java/com/nutriia/nutriia/activities/FoodCompositionActivity.java
+++ app/src/main/java/com/nutriia/nutriia/activities/FoodCompositionActivity.java
@@ -1,6 +1,9 @@
 package com.nutriia.nutriia.activities;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -11,11 +14,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.nutriia.nutriia.R;
 import com.nutriia.nutriia.adapters.FoodCompositionAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 
 public class FoodCompositionActivity extends AppCompatActivity {
+
+    private RecyclerView macroNutrientsRecyclerView;
+    private RecyclerView microNutrientsRecyclerView;
+    private FoodCompositionAdapter macroNutrientAdapter;
+    private FoodCompositionAdapter microNutrientAdapter;
+    private List<String> macroNutrients;
+    private List<String> microNutrients;
+    private TextView foodCalorie;
+    private TextView foodName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,27 +53,88 @@ public class FoodCompositionActivity extends AppCompatActivity {
         ImageButton backButton = findViewById(R.id.lateral_open);
         backButton.setOnClickListener(click -> finish());
 
+        foodCalorie = findViewById(R.id.food_calorie);
+        foodName = findViewById(R.id.food_name);
 
-        RecyclerView macroNutrientsRecyclerView = findViewById(R.id.macronutrients);
+        macroNutrientsRecyclerView = findViewById(R.id.macronutrients);
         macroNutrientsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        List<String> macroNutrients = new ArrayList<>();
-        macroNutrients.add("Protein");
-        macroNutrients.add("Carbs");
-        macroNutrients.add("Fat");
-
-        FoodCompositionAdapter macroNutrientAdapter = new FoodCompositionAdapter(macroNutrients);
+        macroNutrients = new ArrayList<>();
+        macroNutrientAdapter = new FoodCompositionAdapter(macroNutrients);
         macroNutrientsRecyclerView.setAdapter(macroNutrientAdapter);
 
-        RecyclerView microNutrientsRecyclerView = findViewById(R.id.micronutrients);
+        microNutrientsRecyclerView = findViewById(R.id.micronutrients);
         microNutrientsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        List<String> microNutrients = new ArrayList<>();
-        microNutrients.add("Vitamin A");
-        microNutrients.add("Vitamin B");
-        microNutrients.add("Vitamin C");
-
-        FoodCompositionAdapter microNutrientAdapter = new FoodCompositionAdapter(microNutrients);
+        microNutrients = new ArrayList<>();
+        microNutrientAdapter = new FoodCompositionAdapter(microNutrients);
         microNutrientsRecyclerView.setAdapter(microNutrientAdapter);
+
+        String foodNameStr = getIntent().getStringExtra("food_name");
+        fetchFoodComposition(foodNameStr);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void fetchFoodComposition(String foodName) {
+        new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... strings) {
+                String foodName = strings[0];
+                try {
+                    String encodedFoodName = URLEncoder.encode(foodName, "UTF-8");
+                    URL url = new URL("https://nutriia.fr/api/ciqual/index.php?food=" + encodedFoodName);
+
+                    HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+
+                    try {
+                        int responseCode = urlConnection.getResponseCode();
+                        if (responseCode == HttpURLConnection.HTTP_OK) {
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                            StringBuilder stringBuilder = new StringBuilder();
+                            String line;
+                            while ((line = bufferedReader.readLine()) != null) {
+                                stringBuilder.append(line).append("\n");
+                            }
+                            bufferedReader.close();
+                            return stringBuilder.toString();
+                        } else {
+                            Log.e("HTTP_ERROR", "Server returned: " + responseCode);
+                            return null;
+                        }
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String response) {
+                if (response != null) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        foodCalorie.setText(jsonResponse.getString("calories"));
+                        FoodCompositionActivity.this.foodName.setText(jsonResponse.getString("food_name"));
+
+                        JSONArray macroArray = jsonResponse.getJSONArray("macro_nutrients");
+                        for (int i = 0; i < macroArray.length(); i++) {
+                            macroNutrients.add(macroArray.getString(i));
+                        }
+                        macroNutrientAdapter.notifyDataSetChanged();
+
+                        JSONArray microArray = jsonResponse.getJSONArray("micro_nutrients");
+                        for (int i = 0; i < microArray.length(); i++) {
+                            microNutrients.add(microArray.getString(i));
+                        }
+                        microNutrientAdapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.e("HTTP_ERROR", "Failed to fetch food composition");
+                }
+            }
+
+        }.execute(foodName);
     }
 }
--- app/src/main/java/com/nutriia/nutriia/fragments/FoodComposition.java
+++ app/src/main/java/com/nutriia/nutriia/fragments/FoodComposition.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,13 +20,26 @@ import com.nutriia.nutriia.activities.FoodCompositionActivity;
 
 public class FoodComposition extends Fragment {
 
+    private EditText editTextPlat;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.component_food_composition, container, false);
 
+        editTextPlat = view.findViewById(R.id.editplat);
+
         LinearLayout layout = view.findViewById(R.id.linearLayoutComposition);
-        layout.setOnClickListener(click -> startActivity(new Intent(getContext(), FoodCompositionActivity.class)));
+        layout.setOnClickListener(click -> {
+            String foodName = editTextPlat.getText().toString().trim();
+            if (!foodName.isEmpty()) {
+                Intent intent = new Intent(getContext(), FoodCompositionActivity.class);
+                intent.putExtra("food_name", foodName);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getContext(), "Veuillez saisir un nom d'aliment", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
