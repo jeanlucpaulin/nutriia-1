1daf186067b01b5095636c436841def9739ced68
GermainDR <naboriangermain@gmail.com>
2024-06-13 11:53:59 +0200

    +

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.nutriia.nutriia"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
