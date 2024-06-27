2540bbd891fb7b1bfe7591a997a1be47766722fa
GermainDR <germain.doglioli-ruppert@etu.unice.fr>
2024-06-27 15:49:41 +0200

    v0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.nutriia.nutriia"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
