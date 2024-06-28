4b8218b671d259efbf9fedbd3d680f67d2096496
jeanlucpaulin <jeanlucpaulin@gmail.com>
2024-06-28 13:54:50 +0200

    Modification du lien invitation d'amis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.nutriia.nutriiaemf"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.7"
+        versionCode = 8
+        versionName = "0.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
